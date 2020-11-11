e824b2542fcb1b00399ed329900f0c048578ba48
jediofgever <fetulahatas1@gmail.com>
2020-11-11 13:28:06 +0800

    clean up code

--- botanbot_bringup/launch/slam_launch.py
+++ botanbot_bringup/launch/slam_launch.py
@@ -1,17 +1,3 @@
-# Copyright (c) 2020 Samsung Research Russia
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import os
 
 from ament_index_python.packages import get_package_share_directory
@@ -35,7 +21,7 @@ def generate_launch_description():
     lifecycle_nodes = ['map_saver']
 
     # Getting directories and launch-files
-    bringup_dir = get_package_share_directory('chiconybot_bringup')
+    bringup_dir = get_package_share_directory('botanbot_bringup')
     slam_toolbox_dir = get_package_share_directory('slam_toolbox')
     slam_launch_file = os.path.join(slam_toolbox_dir, 'launch', 'online_sync_launch.py')
 
