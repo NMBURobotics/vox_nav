f465b63d4904859783448c128a7b759bebaf7381
jediofgever <fetulahatas1@gmail.com>
2020-11-12 17:35:40 +0800

    configure rbot_ocalization, lgtm for empty GZ wrld

---
+++ botanbot_localization/launch/dual_ekf_navsat.launch.py
@@ -0,0 +1,76 @@
+# Copyright 2018 Open Source Robotics Foundation, Inc.
+# Copyright 2020 Fetullah Atas
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from launch import LaunchDescription
+import launch_ros.actions
+import os
+import yaml
+from launch.substitutions import EnvironmentVariable
+import pathlib
+import launch.actions
+from launch.actions import DeclareLaunchArgument
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    botanbot_localization_dir = get_package_share_directory('botanbot_localization')
+    parameters_file_dir = os.path.join(botanbot_localization_dir, 'params')
+    parameters_file_path = os.path.join(parameters_file_dir, 'dual_ekf_navsat.yaml')
+    os.environ['FILE_PATH'] = str(parameters_file_dir)
+    return LaunchDescription([
+        launch.actions.DeclareLaunchArgument(
+            'output_final_position',
+            default_value='false'),
+        launch.actions.DeclareLaunchArgument(
+            'output_location',
+	    default_value='/home/atas/ekf1.txt'),
+	
+
+	launch_ros.actions.Node(
+            package='robot_localization', 
+            executable='ekf_node', 
+            name='ekf_filter_node_odom',
+	        output='screen',
+            prefix=['gdb -ex run --args'],
+            parameters=[
+                parameters_file_path,
+                str(parameters_file_path),
+                [EnvironmentVariable(name='FILE_PATH'), os.sep, 'dual_ekf_navsat.yaml'],
+           ],
+           ),
+    launch_ros.actions.Node(
+            package='robot_localization', 
+            executable='ekf_node', 
+            name='ekf_filter_node_map',
+	        output='screen',
+            prefix=['gdb -ex run --args'],
+            parameters=[
+                parameters_file_path,
+                str(parameters_file_path),
+                [EnvironmentVariable(name='FILE_PATH'), os.sep, 'dual_ekf_navsat.yaml'],
+           ],
+           ),
+    launch_ros.actions.Node(
+            package='robot_localization', 
+            executable='navsat_transform_node', 
+            name='navsat_transform',
+	        output='screen',
+            prefix=['gdb -ex run --args'],
+            parameters=[
+                parameters_file_path,
+                str(parameters_file_path),
+                [EnvironmentVariable(name='FILE_PATH'), os.sep, 'dual_ekf_navsat.yaml'],
+           ],
+           )
+])
