5c09f481d006841077d3e1840592585712316206
jediofgever <fetulahatas1@gmail.com>
2020-11-16 15:36:19 +0800

    update rviz aunch directory from gui

--- botanbot_gui/scripts/rviz.sh
+++ botanbot_gui/scripts/rviz.sh
@@ -1,3 +1,3 @@
 source ~/colcon_ws/install/setup.bash
 source ~/.bashrc
-gnome-terminal -x sh -c "ros2 launch botanbot_bringup rviz_launch.py; bash" 
+gnome-terminal -x sh -c "ros2 launch botanbot_navigation2 rviz_launch.py; bash" 
