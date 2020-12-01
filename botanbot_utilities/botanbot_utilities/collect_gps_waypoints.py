6a0f5d7618efafc3da8f60e514fea31213501898
jediofgever <fetulahatas1@gmail.com>
2020-12-01 17:45:50 +0800

    sync

--- botanbot_utilities/botanbot_utilities/collect_gps_waypoints.py
+++ botanbot_utilities/botanbot_utilities/collect_gps_waypoints.py
@@ -9,12 +9,13 @@ import os
 from ruamel.yaml import YAML
 import numpy as np
 import sys
+from threading import Thread, Lock
 
-kFrequencyToSaveGPSPoint = 2.0
+kFrequencyToSaveGPSPoint = 0.1
 
 
 class GPSWaypoitCollector(Node):
-
+        
     def __init__(self):
         super().__init__('gps_waypoint_collector')
 
@@ -24,20 +25,26 @@ class GPSWaypoitCollector(Node):
             self.listener_callback, qos_profile_sensor_data
         )
         self.subscription  # prevent unused variable warning
-        self.timer = self.create_timer(
-            kFrequencyToSaveGPSPoint, self.peroidic_callback)
+        self.timer = self.create_timer(kFrequencyToSaveGPSPoint, self.peroidic_callback)
         self.latest_navsat = NavSatFix
         self.index = 0
+        self.mutex = Lock()
 
     def listener_callback(self, msg):
+        self.mutex.acquire()
         self.latest_navsat = msg
+        self.mutex.release()
+
 
     def peroidic_callback(self):
+        self.mutex.acquire()
         print('gps_waypoint'+str(self.index)+':', [self.latest_navsat.latitude,
                                                    self.latest_navsat.longitude,
                                                    self.latest_navsat.altitude])
         input("PRESS ENTER TO COLLECT NEXT WAYPOINT ")
         self.index = self.index+1
+        self.mutex.release()
+
 
 
 def main(args=None):
