ae38413824684503bb6ee25664e09030f0f5fba5
jediofgever <fetulahatas1@gmail.com>
2020-12-17 16:05:21 +0800

    Add a few corrections for cartogrpaher

--- botanbot_cartographer/config/botanbot_lds_3d.lua
+++ botanbot_cartographer/config/botanbot_lds_3d.lua
@@ -1,3 +1,4 @@
+
 -- Copyright 2016 The Cartographer Authors
 --
 -- Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,43 +15,47 @@
 
 include "map_builder.lua"
 include "trajectory_builder.lua"
-
 options = {
-  map_builder = MAP_BUILDER,
-  trajectory_builder = TRAJECTORY_BUILDER,
-  map_frame = "map",
-  tracking_frame = "imu_link",
-  published_frame = "base_link",
-  odom_frame = "odom",
-  provide_odom_frame = true,
-  publish_frame_projected_to_2d = false,
-  use_odometry = true,
-  use_nav_sat = false,
-  use_landmarks = false,
-  num_laser_scans = 0,
-  num_multi_echo_laser_scans = 0,
-  num_subdivisions_per_laser_scan = 1,
-  num_point_clouds = 1,
-  lookup_transform_timeout_sec = 0.2,
-  submap_publish_period_sec = 0.3,
-  pose_publish_period_sec = 5e-3,
-  trajectory_publish_period_sec = 30e-3,
-  rangefinder_sampling_ratio = 1.,
-  odometry_sampling_ratio = 1.,
-  fixed_frame_pose_sampling_ratio = 1.,
-  imu_sampling_ratio = 1.,
-  landmarks_sampling_ratio = 1.,
+ map_builder = MAP_BUILDER,
+ trajectory_builder = TRAJECTORY_BUILDER,
+ map_frame = "map",
+ tracking_frame = "imu_link",
+ published_frame = "base_link",
+ odom_frame = "odom",
+ provide_odom_frame = false,
+ publish_frame_projected_to_2d = false,
+ use_odometry = false,
+ use_nav_sat = false,
+ use_landmarks = false,
+ num_laser_scans = 0,
+ num_multi_echo_laser_scans = 0,
+ num_subdivisions_per_laser_scan = 1,
+ num_point_clouds = 1,
+ lookup_transform_timeout_sec = 0.2,
+ submap_publish_period_sec = 0.3,
+ pose_publish_period_sec = 5e-3,
+ trajectory_publish_period_sec = 30e-3,
+ rangefinder_sampling_ratio = 1.,
+ odometry_sampling_ratio = 1.,
+ fixed_frame_pose_sampling_ratio = 1.,
+ imu_sampling_ratio = 1.,
+ landmarks_sampling_ratio = 1.,
 }
 
-TRAJECTORY_BUILDER_3D.num_accumulated_range_data = 0.1
+TRAJECTORY_BUILDER_3D.num_accumulated_range_data = 1
+TRAJECTORY_BUILDER_3D.min_range = 1.0
+TRAJECTORY_BUILDER_3D.max_range = 150.
+TRAJECTORY_BUILDER_3D.use_online_correlative_scan_matching = true
 
 MAP_BUILDER.use_trajectory_builder_3d = true
-MAP_BUILDER.num_background_threads = 7
+MAP_BUILDER.num_background_threads = 8
 POSE_GRAPH.optimization_problem.huber_scale = 5e2
-POSE_GRAPH.optimize_every_n_nodes = 2
+POSE_GRAPH.optimization_problem.log_solver_summary = true
+POSE_GRAPH.optimize_every_n_nodes = 40
+POSE_GRAPH.optimization_problem.ceres_solver_options.max_num_iterations = 20
 POSE_GRAPH.constraint_builder.sampling_ratio = 0.03
-POSE_GRAPH.optimization_problem.ceres_solver_options.max_num_iterations = 10
-POSE_GRAPH.constraint_builder.min_score = 0.62
-POSE_GRAPH.constraint_builder.global_localization_min_score = 0.66
+POSE_GRAPH.constraint_builder.min_score = 0.5
+POSE_GRAPH.constraint_builder.global_localization_min_score = 0.55
+
 
 return options
