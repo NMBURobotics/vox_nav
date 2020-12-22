1c70f2de852af8fdf70ca1d80924a290aca5888b
jediofgever <fetulahatas1@gmail.com>
2020-12-22 17:08:58 +0800

    update sphinx theme

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'OUTDOOR_NAV2'
-copyright = '2020, Fetullah Atas'
+copyright = '2020, Fetullah Atas, Norwegian University of Life Sciences'
 author = 'Fetullah Atas'
 
 master_doc = 'index'
@@ -45,7 +45,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
