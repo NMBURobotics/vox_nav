ca1217a65bdf847f129cc83dcabcd33ebf470cff
jediofgever <fetulahatas1@gmail.com>
2021-01-19 13:13:07 +0800

    comment code

--- botanbot_map_server/include/botanbot_map_server/botanbot_map_manager.hpp
+++ botanbot_map_server/include/botanbot_map_server/botanbot_map_manager.hpp
@@ -25,6 +25,7 @@
 #include <sensor_msgs/msg/point_cloud2.hpp>
 #include <botanbot_utilities/navsat_conversions.hpp>
 #include <botanbot_utilities/pcl_helpers.hpp>
+#include <botanbot_utilities/gps_waypoint_collector.hpp>
 #include <botanbot_msgs/msg/oriented_nav_sat_fix.hpp>
 
 #include <octomap_msgs/msg/octomap.hpp>
@@ -91,26 +92,43 @@ public:
   void publishUTMMapTransfrom();
 
 protected:
+  // Used to creted a periodic callback function IOT publish transfrom/octomap/cloud etc.
   rclcpp::TimerBase::SharedPtr timer_;
+  // publishes octomap in its native format
   rclcpp::Publisher<octomap_msgs::msg::Octomap>::SharedPtr octomap_publisher_;
+  // reusable octomp message, dont need to recreate each time we publish
   octomap_msgs::msg::Octomap::SharedPtr octomap_ros_msg_;
-
+  // publishes octomap in form of a point cloud message
   rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr octomap_pointloud_publisher_;
+  // reusable octomap point loud message, dont need to recreate each time we publish
   sensor_msgs::msg::PointCloud2::SharedPtr octomap_pointcloud_ros_msg_;
+  // otree object to read and store binary octomap from disk
   std::shared_ptr<octomap::OcTree> octomap_octree_;
-
+  // we read gps coordinates of map from yaml
   botanbot_msgs::msg::OrientedNavSatFix::SharedPtr oriented_navsat_fix_ros_msg_;
+  // to broadcast utm -> map
   tf2_ros::TransformBroadcaster tf_broadcaster_;
-  botanbot_utilities::RigidBodyTransformation pointloud_transform_matrix_;
-
+  // rclcpp parameters from yaml file: full path to octomap file in disk
   std::string octomap_filename_;
+  // rclcpp parameters from yaml file: topic name for published octomap
   std::string octomap_publish_topic_name_;
+  // rclcpp parameters from yaml file: vxel size for octomap
   double octomap_voxel_size_;
+  // rclcpp parameters from yaml file: publish frequncy to publish map and transfroms
   int octomap_publish_frequency_;
+  // rclcpp parameters from yaml file: if true, utm -> map transftom is avail for TF
   bool provide_utm_to_map_transform_;
+  // rclcpp parameters from yaml file: if true, a cloud will be published which represents octomap
   bool publish_octomap_as_pointcloud_;
+  // rclcpp parameters from yaml file: topic name for published octomap as cloud
   std::string octomap_point_cloud_publish_topic_;
+  // rclcpp parameters from yaml file: frame id for octomap typicall: "map"
   std::string octomap_frame_id_;
+  // We will get the GPS coordinates before starting the Mapping.
+  std::shared_ptr<botanbot_utilities::GPSWaypointCollector> gps_waypoint_collector_node_;
+  // we will recieve gp data once and that is it , weonly need this to precisely
+  // define start location of map
+  std::once_flag gps_data_recieved_flag_;
 };
 }  // namespace botanbot_map_server
 
