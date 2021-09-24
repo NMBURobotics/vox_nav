9e50a565d9781bae162dfb293796e9ce07408aed
jediofgever <fetulahatas1@gmail.com>
2021-09-24 11:16:44 +0200

    going to branch up fro MPC

--- vox_nav_utilities/scripts/attamts_stomp.py
+++ vox_nav_utilities/scripts/attamts_stomp.py
@@ -20,17 +20,19 @@ R = np.linalg.inv(np.matmul(np.transpose(A), A))
 M = np.linalg.inv(R)
 print(M)
 
-kernel_size = 5
+iterations = 10000
+sobel_kernel_size = 7
+dt_kernel_size = 5
 step = 10
-start = [0, 0]
-goal = [700, 700]
+start = [40, 60]
+goal = [800, 60]
 waypoints = [[0, 0]]
 
 for s in range(0, goal[0], step):
-    waypoints.append([s, s])
+    waypoints.append([s, 60])
 waypoints.append(goal)
 
-costmap = cv.imread("aruco.png")
+costmap = cv.imread("map.png")
 gray_costmap = cv.cvtColor(costmap, cv.COLOR_BGR2GRAY)
 
 binary_thresh_inverted = cv.threshold(
@@ -39,25 +41,25 @@ binary_thresh_inverted = cv.threshold(
 binary_thresh = cv.threshold(gray_costmap, 0, 250, cv.THRESH_BINARY)[1]
 
 distance_transfrom_inverted = cv.distanceTransform(
-    binary_thresh_inverted, distanceType=cv.DIST_L2, maskSize=kernel_size)
+    binary_thresh_inverted, distanceType=cv.DIST_L2, maskSize=dt_kernel_size)
 
 distance_transfrom = cv.distanceTransform(
-    binary_thresh, distanceType=cv.DIST_L2, maskSize=kernel_size)
+    binary_thresh, distanceType=cv.DIST_L2, maskSize=dt_kernel_size)
 
 distance_transfrom_inverted = cv.normalize(
-    distance_transfrom_inverted, distance_transfrom_inverted, 0.0, 1.0, cv.NORM_MINMAX)
+    distance_transfrom_inverted, distance_transfrom_inverted, 0.0, 0.8, cv.NORM_MINMAX)
 
 distance_transfrom = cv.normalize(
-    distance_transfrom, distance_transfrom, 0, 1.0, cv.NORM_MINMAX)
+    distance_transfrom, distance_transfrom, 0, 0.8, cv.NORM_MINMAX)
 
 np.set_printoptions(threshold=sys.maxsize)
 
 gradx_dti = cv.Sobel(distance_transfrom_inverted,
-                     cv.CV_32F, 1, 0, ksize=kernel_size)
+                     cv.CV_32F, 1, 0, ksize=sobel_kernel_size)
 grady_dti = cv.Sobel(distance_transfrom_inverted,
-                     cv.CV_32F, 0, 1, ksize=kernel_size)
-gradx_dt = cv.Sobel(distance_transfrom, cv.CV_32F, 1, 0, ksize=kernel_size)
-grady_dt = cv.Sobel(distance_transfrom, cv.CV_32F, 0, 1, ksize=kernel_size)
+                     cv.CV_32F, 0, 1, ksize=sobel_kernel_size)
+gradx_dt = cv.Sobel(distance_transfrom, cv.CV_32F, 1, 0, ksize=sobel_kernel_size)
+grady_dt = cv.Sobel(distance_transfrom, cv.CV_32F, 0, 1, ksize=sobel_kernel_size)
 
 gradmag_dti = cv.magnitude(gradx_dti, grady_dti)
 gradmag_dt = cv.magnitude(gradx_dt, grady_dt)
@@ -65,8 +67,8 @@ gradmag_dt = cv.magnitude(gradx_dt, grady_dt)
 # mag, angle = cv.cartToPolar(gradx, grady, angleInDegrees=True)
 # mag, angle = cv.cartToPolar(gradx, grady, angleInDegrees=True)
 
-for i in range(0, gradx_dt.shape[0], 1):
-    for j in range(0, grady_dt.shape[0], 1):
+for i in range(0, gradx_dt.shape[0]):
+    for j in range(0, grady_dt.shape[1]):
         gradx_dt[i, j] -= gradx_dti[i, j]
         grady_dt[i, j] -= grady_dti[i, j]
 
@@ -90,7 +92,6 @@ cv.polylines(distance_transfrom, np.int32([waypoints]), 0, (255, 0, 255))
 
 updated_path = []
 
-iterations = 100
 for j in range(0, iterations):
     for i in range(0, len(waypoints)):
         if j == 0:
@@ -101,7 +102,7 @@ for j in range(0, iterations):
         updated_path[i][0] += gradx_dt[waypoints[i][1], waypoints[i][0]]
         updated_path[i][1] += grady_dt[waypoints[i][1], waypoints[i][0]]
 
-        if j % 10 == 0:
+        if j % 1 == 0:
             waypoints[i][0] = int(updated_path[i][0])
             waypoints[i][1] = int(updated_path[i][1])
 
