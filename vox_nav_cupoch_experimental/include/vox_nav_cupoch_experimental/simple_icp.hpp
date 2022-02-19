712379db55b88c903007578197ef6994a349bff6
Fetullah Atas <fetulahatas1@gmail.com>
2022-02-19 15:41:19 +0100

    add simple icp

---
+++ vox_nav_cupoch_experimental/include/vox_nav_cupoch_experimental/simple_icp.hpp
@@ -0,0 +1,112 @@
+// Copyright (c) 2021 Fetullah Atas, Norwegian University of Life Sciences
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#ifndef VOX_NAV_CUPOCH_EXPERIMENTAL__SIMPLE_ICP_HPP_
+#define VOX_NAV_CUPOCH_EXPERIMENTAL__SIMPLE_ICP_HPP_
+
+#include <pcl/PCLPointCloud2.h>
+#include <pcl/common/common.h>
+#include <pcl/conversions.h>
+#include <pcl/filters/crop_box.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl_ros/transforms.hpp>
+
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/imu.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <tf2_eigen/tf2_eigen.h>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
+#include <visualization_msgs/msg/marker_array.hpp>
+#include <pcl/filters/model_outlier_removal.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <geometry_msgs/msg/pose_array.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+
+#include <cupoch/collision/collision.h>
+#include <cupoch/cupoch.h>
+#include <cupoch/geometry/occupancygrid.h>
+#include <cupoch_conversions/cupoch_conversions.hpp>
+
+#include "vox_nav_utilities/map_manager_helpers.hpp"
+#include "vox_nav_utilities/pcl_helpers.hpp"
+#include "vox_nav_utilities/tf_helpers.hpp"
+#include "vox_nav_msgs/msg/object.hpp"
+#include "vox_nav_msgs/msg/object_array.hpp"
+
+#include <queue>
+#include <vector>
+#include <string>
+#include <memory>
+
+#include <Eigen/Core>
+
+namespace vox_nav_cupoch_experimental
+{
+
+/**
+ * @brief Given a raw point cloud,
+ * clusterize it and use UKF to track clusters. Publish vis of tracks in RVIZ
+ * and publish vox_nav_msgs::msg::ObjectArray
+ *
+ */
+  class SimpleICP : public rclcpp::Node
+  {
+
+  public:
+    /**
+     * @brief Construct a new Raw Cloud Clustering Tracking object
+     *
+     */
+    SimpleICP();
+    /**
+     * @brief Destroy the Raw Cloud Clustering Tracking object
+     *
+     */
+    ~SimpleICP();
+
+    /**
+     * @brief Processing done in this func.
+     *
+     * @param cloud
+     * @param poses
+     */
+    void cloudCallback(
+      const sensor_msgs::msg::PointCloud2::ConstSharedPtr cloud);
+
+     /**
+     * @brief Processing done in this func.
+     *
+     * @param cloud
+     * @param poses
+     */
+    void map
+    mapCloudCallback(
+      const sensor_msgs::msg::PointCloud2::ConstSharedPtr cloud);
+
+  private:
+
+    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr cloud_subscriber_;
+    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr map_cloud_subscriber_;
+
+  };
+
+}  // namespace vox_nav_cupoch_experimental
+
+#endif  // VOX_NAV_CUPOCH_EXPERIMENTAL__SIMPLE_ICP_HPP_
---
+++ vox_nav_cupoch_experimental/src/simple_icp.cu
@@ -0,0 +1,59 @@
+// Copyright (c) 2020 Fetullah Atas, Norwegian University of Life Sciences
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*
+DISCLAIMER: some parts of code has been taken from; https://github.com/appinho/SARosPerceptionKitti
+Credits to author: Simon Appel, https://github.com/appinho
+*/
+
+#include "vox_nav_cupoch_experimental/simple_icp.hpp"
+
+using namespace vox_nav_cupoch_experimental;
+
+SimpleICP::SimpleICP()
+: Node("cloud_clustering_rclcpp_node")
+{
+  
+  cloud_subscriber_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
+    "/ouster/points",
+    rclcpp::SensorDataQoS(),
+    std::bind(
+      &SimpleICP::cloudCallback,  this, std::placeholders::_1));
+
+  map_cloud_subscriber_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
+    "/map/points",
+    rclcpp::SensorDataQoS(),
+    std::bind(
+      &SimpleICP::mapCloudCallback,  this, std::placeholders::_1));
+
+  /*cloud_clusters_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(
+    "/vox_nav/detection/clusters", rclcpp::SystemDefaultsQoS());*/
+
+
+  RCLCPP_INFO(get_logger(), "Creating...");
+}
+
+SimpleICP::~SimpleICP()
+{
+  RCLCPP_INFO(get_logger(), "Destroying...");
+}
+
+int main(int argc, char const * argv[])
+{
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<SimpleICP>();
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
+}
