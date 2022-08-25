488ea1097b5ff21cc12e0066c0233b3c0210f684
jediofgever <fetulahatas1@gmail.com>
2022-08-25 17:21:41 +0200

    leanup polytunnel planner

--- vox_nav_planning/include/vox_nav_planning/plugins/polytunnel_planner.hpp
+++ vox_nav_planning/include/vox_nav_planning/plugins/polytunnel_planner.hpp
@@ -41,17 +41,26 @@ namespace vox_nav_planning
   class PolyTunnelPlanner : public vox_nav_planning::PlannerCore
   {
 
+    // Declaring the type of Predicate that accepts 2 pairs and return a bool
+    typedef std::function<bool (std::pair<int, double>, std::pair<int, double>)> Comparator;
+    // Defining a lambda function to compare two pairs. It will compare two pairs using second field
+    Comparator compFunctor =
+      [](std::pair<int, double> elem1, std::pair<int, double> elem2)
+      {
+        return elem1.second < elem2.second;
+      };
+
   public:
-/**
- * @brief Construct a new PolyTunnelPlanner object
- *
- */
+    /**
+     * @brief Construct a new PolyTunnelPlanner object, a plugin to create path plans for polytunnels of straberries
+     *
+     */
     PolyTunnelPlanner();
 
-/**
- * @brief Destroy the PolyTunnelPlanner object
- *
- */
+    /**
+     * @brief Destroy the PolyTunnelPlanner object
+     *
+     */
     ~PolyTunnelPlanner();
 
     /**
@@ -74,21 +83,21 @@ namespace vox_nav_planning
       const geometry_msgs::msg::PoseStamped & goal) override;
 
     /**
-     * @brief Get the Overlayed Startand Goal object
+     * @brief Get the Overlayed Startand Goal object, not implemented
      *
      * @return std::vector<geometry_msgs::msg::PoseStamped>
      */
     std::vector<geometry_msgs::msg::PoseStamped> getOverlayedStartandGoal() override;
 
     /**
-     * @brief
+     * @brief Get the selected poitcloud, this should roughly include the strberry rows. Use SelectPointPoblisheer RVIZ pluginn
      *
      * @param msg
      */
     void polytunnelCloudCallback(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
 
     /**
-     * @brief Not implemented
+     * @brief Not implemented, we use pointcloud coming from @polytunnelCloudCallback
      *
      */
     void setupMap() override
@@ -106,10 +115,212 @@ namespace vox_nav_planning
     {
     }
 
+    cv::Mat getCloudSideViewImage(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, double resolution)
+    {
+      pcl::PointXYZ minPt, maxPt;
+      pcl::getMinMax3D(*cloud, minPt, maxPt);
+      double dy = maxPt.y - minPt.y;
+      double dz = maxPt.z - minPt.z;
+      int dimx = static_cast<int>(dy / resolution);
+      int dimy = static_cast<int>(dz / resolution);
+      auto image = cv::Mat(
+        dimy,
+        dimx,
+        CV_8UC3);
+      image.setTo(cv::Scalar(255, 255, 255));
+      for (auto && point : cloud->points) {
+        int x = static_cast<int>((maxPt.y - point.y) / resolution);
+        int y = static_cast<int>((maxPt.z - point.z) / resolution);
+        if ((x >= 0 && x <= dimx) && (y >= 0 && y <= dimy)  ) {
+          image.at<cv::Vec3b>(y, x)[0] = 255;
+          image.at<cv::Vec3b>(y, x)[1] = 0;
+          image.at<cv::Vec3b>(y, x)[2] = 0;
+        }
+      }
+      return image;
+    }
+
+    std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> getClustersWithinBBXs(
+      pcl::PointCloud<pcl::PointXYZ>::Ptr cloud,
+      std::vector<cv::Rect> boundingBoxes,
+      double resolution,
+      double row_cloud_downsample_size,
+      pcl::PointXYZRGB & average_point
+    )
+    {
+      std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> clusters;
+      pcl::PointXYZ minPt, maxPt;
+      pcl::getMinMax3D(*cloud, minPt, maxPt);
+
+      int bbx_idx = 0;
+
+      for (auto && rect : boundingBoxes) {
+
+        pcl::PointCloud<pcl::PointXYZRGB>::Ptr curr_cluster(new pcl::PointCloud<pcl::PointXYZRGB>());
+
+        for (auto && point : cloud->points) {
+          int x = static_cast<int>((maxPt.y - point.y) / resolution);
+          int y = static_cast<int>((maxPt.z - point.z) / resolution);
+
+          if (x >= rect.x && x <= rect.x + rect.width) {
+            if (y >= rect.y && y <= rect.y + rect.height) {
+              pcl::PointXYZRGB selected_p;
+              selected_p.x = point.x;
+              selected_p.y = point.y;
+              selected_p.z = point.z;
+              selected_p.r = vox_nav_utilities::getColorByIndexEig(bbx_idx).x() * 255.0;
+              selected_p.g = vox_nav_utilities::getColorByIndexEig(bbx_idx).y() * 255.0;
+              selected_p.b = vox_nav_utilities::getColorByIndexEig(bbx_idx).z() * 255.0;
+              curr_cluster->points.push_back(selected_p);
+            }
+          }
+        }
+        pcl::PointXYZRGB center;
+        pcl::computeCentroid<pcl::PointXYZRGB, pcl::PointXYZRGB>(*curr_cluster, center);
+        pcl::PointXYZ center_xyz(center.x, center.y, center.z);
+        average_point.z = center.z;
+
+        for (auto && i : curr_cluster->points) {
+          double dy = i.y - center.y;
+          double dz = i.z - center.z;
+          i.y -= dy;
+          i.z -= dz;
+        }
+        curr_cluster = vox_nav_utilities::downsampleInputCloud<pcl::PointXYZRGB>(
+          curr_cluster,
+          row_cloud_downsample_size);
+        clusters.push_back(curr_cluster);
+        bbx_idx++;
+      }
+
+      return clusters;
+    }
+
+    std::map<int, double> getClusterDistancetoRobot(
+      std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> clusters,
+      pcl::PointXYZ robot_position)
+    {
+      int bbx_idx = 0;
+      std::map<int, double> src;
+      for (auto && curr_cluster : clusters) {
+        pcl::PointXYZRGB minP, maxP;
+        pcl::getMinMax3D<pcl::PointXYZRGB>(*curr_cluster, minP, maxP);
+        double dist_d = vox_nav_utilities::PCLPointEuclideanDist<pcl::PointXYZ>(
+          pcl::PointXYZ(minP.x, minP.y, minP.z),
+          robot_position);
+        src.insert(std::pair<int, double>(bbx_idx, dist_d));
+        bbx_idx++;
+      }
+      return src;
+    }
+
+    std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> orginizeAndSortClusterPoints(
+      std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> clusters,
+      std::set<std::pair<int, double>, Comparator> set,
+      pcl::PointXYZ robot_position)
+    {
+      int curr_row = 0;
+
+      // Given selected bounding boxes, find points that lay inside this
+      std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> clusters_organized;
+
+      for (auto const & pair: set) {
+        auto comp = [&](const pcl::PointXYZ & a, const pcl::PointXYZ & b) {
+            double dist_a = vox_nav_utilities::PCLPointEuclideanDist<pcl::PointXYZ>(
+              a,
+              robot_position);
+            double dist_b = vox_nav_utilities::PCLPointEuclideanDist<pcl::PointXYZ>(
+              b,
+              robot_position);
+            return dist_a > dist_b;
+          };
+
+        std::priority_queue<pcl::PointXYZ, std::vector<pcl::PointXYZ>, decltype(comp)> priQue(comp);
+
+        for (unsigned int i = 0; i < clusters[pair.first]->points.size(); ++i) {
+          pcl::PointXYZRGB point = clusters[pair.first]->points[i];
+          pcl::PointXYZ point_xyz(point.x, point.y, point.z);
+          priQue.push(point_xyz);
+        }
+
+        pcl::PointCloud<pcl::PointXYZ>::Ptr curr_cluster_organized(
+          new pcl::PointCloud<pcl::PointXYZ>());
+
+        while (!priQue.empty()) {
+          const pcl::PointXYZ & point = priQue.top();
+          curr_cluster_organized->points.push_back(point);
+          priQue.pop();
+        }
+        clusters_organized.push_back(curr_cluster_organized);
+
+        curr_row++;
+      }
+
+      return clusters_organized;
+
+    }
+
+    std::vector<geometry_msgs::msg::PoseStamped> rowClusters2InterpolatedPath(
+      std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> clusters_organized,
+      int extra_interpolation,
+      pcl::PointXYZRGB average_point
+    )
+    {
+      std::vector<geometry_msgs::msg::PoseStamped> plan_poses;
+      ompl::geometric::PathGeometric path(state_space_information_);
+
+      bool straight_flag = true;
+      for (auto && i : clusters_organized) {
+
+        if (!straight_flag) {
+          std::reverse(i->points.begin(), i->points.end());
+        }
+
+        for (auto && p : i->points) {
+          double void_var, yaw;
+          if (!straight_flag) {yaw = M_PI;}
+          ompl::base::ScopedState<> this_state(state_space_information_->getStateSpace());
+          this_state[0] = p.x;
+          this_state[1] = p.y;
+          this_state[2] = yaw;
+          path.append(static_cast<ompl::base::State *>(this_state.get()));
+        }
+
+        straight_flag = !straight_flag;
+      }
+
+      path.interpolate(plan_poses.size() + extra_interpolation);
+
+      for (std::size_t path_idx = 0; path_idx < path.getStateCount(); path_idx++) {
+        // cast the abstract state type to the type we expect
+        const ompl::base::DubinsStateSpace::StateType * interpolated_state =
+          path.getState(path_idx)->as<ompl::base::DubinsStateSpace::StateType>();
+
+        geometry_msgs::msg::PoseStamped pose;
+        pose.header.frame_id = polytunnel_cloud_.header.frame_id;
+        pose.header.stamp = rclcpp::Clock().now();
+        pose.pose.position.x = interpolated_state->getX();
+        pose.pose.position.y = interpolated_state->getY();
+        pose.pose.position.z = average_point.z;
+        pose.pose.orientation = vox_nav_utilities::getMsgQuaternionfromRPY(
+          0, 0, interpolated_state->getYaw());
+
+        plan_poses.push_back(pose);
+      }
+
+      return plan_poses;
+    }
+
   protected:
-    rclcpp::Logger logger_{rclcpp::get_logger("elevation_control_planner")};
+    rclcpp::Logger logger_ {rclcpp::get_logger("polytunnel_planner")};
+
+    // Subscribe to RRVIZ plugin to get the selected area's point cloud
     rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr polytunnel_cloud_sub_;
+
+    // Publish detected rows as point cloud clusters with differnt colors
     rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr polytunnel_cloud_pub_;
+
+    // Global member to keep most up to date selectd point cloud
     sensor_msgs::msg::PointCloud2 polytunnel_cloud_;
 
     // tf buffer to get transfroms
@@ -117,12 +328,18 @@ namespace vox_nav_planning
     // tf listner for tf transforms
     std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
 
-
+    // OMPL utilities used fo path interpolation, to connect rows to each other
     ompl::base::StateSpacePtr state_space_;
     std::string selected_se2_space_name_;
     // curve radius for reeds and dubins only
     double rho_;
     ompl::base::SpaceInformationPtr state_space_information_;
+
+    double transform_timeout_;
+    double resolution_;
+    double row_cloud_downsample_size_;
+    int extra_interpolation_;
+
   };
 }  // namespace vox_nav_planning
 
--- vox_nav_planning/src/plugins/polytunnel_planner.cpp
+++ vox_nav_planning/src/plugins/polytunnel_planner.cpp
@@ -19,7 +19,6 @@
 #include <memory>
 #include <vector>
 #include <random>
-#include <boost/range/adaptor/reversed.hpp>
 
 namespace vox_nav_planning
 {
@@ -42,19 +41,26 @@ namespace vox_nav_planning
       std::bind(&PolyTunnelPlanner::polytunnelCloudCallback, this, std::placeholders::_1));
 
     polytunnel_cloud_pub_ = parent->create_publisher<sensor_msgs::msg::PointCloud2>(
-      "clusterized",
+      "/detected_tunnel_rows",
       rclcpp::SystemDefaultsQoS());
 
     tf_buffer_ = std::make_unique<tf2_ros::Buffer>(parent->get_clock());
     tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
 
-    RCLCPP_INFO(logger_, "Selected planner is: %s", planner_name_.c_str());
+    parent->declare_parameter(plugin_name + ".ref_traj_se2_space", "DUBINS");
+    parent->declare_parameter(plugin_name + ".rho", 1.0);
+    parent->declare_parameter(plugin_name + ".transform_timeout", 0.1);
+    parent->declare_parameter(plugin_name + ".resolution", 0.01);
+    parent->declare_parameter(plugin_name + ".row_cloud_downsample_size", 0.4);
+    parent->declare_parameter(plugin_name + ".extra_interpolation", 60);
 
-    parent->declare_parameter("ref_traj_se2_space", "DUBINS");
-    parent->declare_parameter("rho", 1.0);
+    parent->get_parameter(plugin_name + ".ref_traj_se2_space", selected_se2_space_name_);
+    parent->get_parameter(plugin_name + ".rho", rho_);
+    parent->get_parameter(plugin_name + ".transform_timeout", transform_timeout_);
+    parent->get_parameter(plugin_name + ".resolution", resolution_);
+    parent->get_parameter(plugin_name + ".row_cloud_downsample_size", row_cloud_downsample_size_);
+    parent->get_parameter(plugin_name + ".extra_interpolation", extra_interpolation_);
 
-    parent->get_parameter("ref_traj_se2_space", selected_se2_space_name_);
-    parent->get_parameter("rho", rho_);
 
     // This is used to interpolate local refernce states
     std::shared_ptr<ompl::base::RealVectorBounds> state_space_bounds =
@@ -71,13 +77,15 @@ namespace vox_nav_planning
     }
     state_space_information_ = std::make_shared<ompl::base::SpaceInformation>(state_space_);
 
+    RCLCPP_INFO(logger_, "Creating...");
+
   }
 
   void PolyTunnelPlanner::polytunnelCloudCallback(
     const sensor_msgs::msg::PointCloud2::SharedPtr msg)
   {
     polytunnel_cloud_ = *msg;
-    RCLCPP_INFO(logger_, "Selected points received");
+    RCLCPP_INFO(logger_, "Selected points received.");
   }
 
 
@@ -95,32 +103,10 @@ namespace vox_nav_planning
     }
     RCLCPP_INFO(logger_, "Creating a plan with %d points", cloud->points.size());
 
-    pcl::PointXYZ minPt, maxPt;
-    pcl::getMinMax3D(*cloud, minPt, maxPt);
+    //Get the side view image of the point cloud
+    cv::Mat image = getCloudSideViewImage(cloud, resolution_);
 
-    double dy = maxPt.y - minPt.y;
-    double dz = maxPt.z - minPt.z;
-
-    double resolution = 0.01;
-
-    int dimx = static_cast<int>(dy / resolution);
-    int dimy = static_cast<int>(dz / resolution);
-
-    auto image = cv::Mat(
-      dimy,
-      dimx,
-      CV_8UC3);
-    image.setTo(cv::Scalar(255, 255, 255));
-
-    for (auto && point : cloud->points) {
-      int x = static_cast<int>((maxPt.y - point.y) / resolution);
-      int y = static_cast<int>((maxPt.z - point.z) / resolution);
-      if ((x >= 0 && x <= dimx) && (y >= 0 && y <= dimy)  ) {
-        image.at<cv::Vec3b>(y, x)[0] = 255;
-        image.at<cv::Vec3b>(y, x)[1] = 0;
-        image.at<cv::Vec3b>(y, x)[2] = 0;
-      }
-    }
+    //Select the rows with bounding boxes
     const cv::String windowName = "Select";
     std::vector<cv::Rect> boundingBoxes;
     bool showCrosshair = true;
@@ -128,189 +114,39 @@ namespace vox_nav_planning
     cv::selectROIs(windowName, image, boundingBoxes, showCrosshair, fromCenter);
     cv::destroyWindow("Select");
 
-    std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> clusters;
-    std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> clusters_organized;
+    pcl::PointXYZRGB average_point;
 
-    int bbx_idx = 0;
+    std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> clusters = getClustersWithinBBXs(
+      cloud,
+      boundingBoxes,
+      resolution_,
+      row_cloud_downsample_size_,
+      average_point);
 
     geometry_msgs::msg::PoseStamped curr_robot_pose;
     vox_nav_utilities::getCurrentPose(
-      curr_robot_pose, *tf_buffer_, "map", "base_link", 0.1);
+      curr_robot_pose, *tf_buffer_, "map", "base_link", transform_timeout_);
     pcl::PointXYZ robot_position(
       curr_robot_pose.pose.position.x,
       curr_robot_pose.pose.position.y,
       curr_robot_pose.pose.position.z);
 
-    std::map<int, double> src;
+    std::map<int, double> cluster_distance_map =
+      getClusterDistancetoRobot(clusters, robot_position);
 
-    pcl::PointXYZRGB average_point;
+    // sort the  map by value,
+    std::set<std::pair<int, double>, Comparator> set(
+      cluster_distance_map.begin(), cluster_distance_map.end(), compFunctor);
 
+    std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> clusters_organized =
+      orginizeAndSortClusterPoints(clusters, set, robot_position);
 
-    for (auto && rect : boundingBoxes) {
+    std::vector<geometry_msgs::msg::PoseStamped> plan_poses = rowClusters2InterpolatedPath(
+      clusters_organized, extra_interpolation_, average_point);
 
-      pcl::PointCloud<pcl::PointXYZRGB>::Ptr curr_cluster(new pcl::PointCloud<pcl::PointXYZRGB>());
-
-      for (auto && point : cloud->points) {
-        int x = static_cast<int>((maxPt.y - point.y) / resolution);
-        int y = static_cast<int>((maxPt.z - point.z) / resolution);
-
-        if (x >= rect.x && x <= rect.x + rect.width) {
-          if (y >= rect.y && y <= rect.y + rect.height) {
-            pcl::PointXYZRGB selected_p;
-            selected_p.x = point.x;
-            selected_p.y = point.y;
-            selected_p.z = point.z;
-            selected_p.r = vox_nav_utilities::getColorByIndexEig(bbx_idx).x() * 255.0;
-            selected_p.g = vox_nav_utilities::getColorByIndexEig(bbx_idx).y() * 255.0;
-            selected_p.b = vox_nav_utilities::getColorByIndexEig(bbx_idx).z() * 255.0;
-            curr_cluster->points.push_back(selected_p);
-          }
-        }
-      }
-      pcl::PointXYZRGB center;
-      pcl::computeCentroid<pcl::PointXYZRGB, pcl::PointXYZRGB>(*curr_cluster, center);
-      pcl::PointXYZ center_xyz(center.x, center.y, center.z);
-
-      average_point = center;
-
-      for (auto && i : curr_cluster->points) {
-        double dy = i.y - center.y;
-        double dz = i.z - center.z;
-        i.y -= dy;
-        i.z -= dz;
-      }
-      curr_cluster = vox_nav_utilities::downsampleInputCloud<pcl::PointXYZRGB>(curr_cluster, 0.4);
-
-      pcl::PointXYZRGB minP, maxP;
-      pcl::getMinMax3D<pcl::PointXYZRGB>(*curr_cluster, minP, maxP);
-
-      double dist_d = vox_nav_utilities::PCLPointEuclideanDist<pcl::PointXYZ>(
-        pcl::PointXYZ(minP.x, minP.y, minP.z),
-        robot_position);
-
-      src.insert(std::pair<int, double>(bbx_idx, dist_d));
-
-      clusters.push_back(curr_cluster);
-      bbx_idx++;
-
-    }
-
-    // Declaring the type of Predicate that accepts 2 pairs and return a bool
-    typedef std::function<bool (std::pair<int, double>, std::pair<int, double>)> Comparator;
-    // Defining a lambda function to compare two pairs. It will compare two pairs using second field
-    Comparator compFunctor =
-      [](std::pair<int, double> elem1, std::pair<int, double> elem2)
-      {
-        return elem1.second < elem2.second;
-      };
-    // Declaring a set that will store the pairs using above comparision logic
-    std::set<std::pair<int, double>, Comparator> set(src.begin(), src.end(), compFunctor);
-
-    // print the vector
-    int curr_row = 0;
-
-    for (auto const & pair: set) {
-
-      std::cout << '{' << pair.first << "," << pair.second << '}' << std::endl;
-
-      auto comp = [&](const pcl::PointXYZ & a, const pcl::PointXYZ & b) {
-          double dist_a = vox_nav_utilities::PCLPointEuclideanDist<pcl::PointXYZ>(
-            a,
-            robot_position);
-          double dist_b = vox_nav_utilities::PCLPointEuclideanDist<pcl::PointXYZ>(
-            b,
-            robot_position);
-          return dist_a > dist_b;
-        };
-
-      std::priority_queue<pcl::PointXYZ, std::vector<pcl::PointXYZ>, decltype(comp)> priQue(comp);
-
-      for (unsigned int i = 0; i < clusters[pair.first]->points.size(); ++i) {
-        pcl::PointXYZRGB point = clusters[pair.first]->points[i];
-        pcl::PointXYZ point_xyz(point.x, point.y, point.z);
-        priQue.push(point_xyz);
-      }
-
-      pcl::PointCloud<pcl::PointXYZ>::Ptr curr_cluster_organized(
-        new pcl::PointCloud<pcl::PointXYZ>());
-
-      while (!priQue.empty()) {
-        const pcl::PointXYZ & point = priQue.top();
-        curr_cluster_organized->points.push_back(point);
-        priQue.pop();
-      }
-      clusters_organized.push_back(curr_cluster_organized);
-
-      curr_row++;
-    }
-
-    std::vector<geometry_msgs::msg::PoseStamped> plan_poses;
-
-    bool straight_flag = true;
-
-    for (auto && i : clusters_organized) {
-
-      if (!straight_flag) {
-        std::reverse(i->points.begin(), i->points.end());
-      }
-
-      for (auto && p : i->points) {
-        geometry_msgs::msg::PoseStamped pose;
-        pose.header.frame_id = polytunnel_cloud_.header.frame_id;
-        pose.header.stamp = rclcpp::Clock().now();
-        pose.pose.position.x = p.x;
-        pose.pose.position.y = p.y;
-        pose.pose.position.z = p.z;
-        if (!straight_flag) {
-          pose.pose.orientation = vox_nav_utilities::getMsgQuaternionfromRPY(0, 0, M_PI);
-        }
-
-        plan_poses.push_back(pose);
-      }
-
-      straight_flag = !straight_flag;
-    }
-
-    ompl::geometric::PathGeometric path(state_space_information_);
-
-
-    for (auto && i : plan_poses) {
-      double void_var, yaw;
-      ompl::base::ScopedState<>
-      this_state(state_space_information_->getStateSpace());
-      vox_nav_utilities::getRPYfromMsgQuaternion(
-        i.pose.orientation, void_var, void_var, yaw);
-      this_state[0] = i.pose.position.x;
-      this_state[1] = i.pose.position.y;
-      this_state[2] = yaw;
-      path.append(static_cast<ompl::base::State *>(this_state.get()));
-    }
-
-    path.interpolate(plan_poses.size() + 60);
-    plan_poses.clear();
-
-    for (std::size_t path_idx = 0; path_idx < path.getStateCount(); path_idx++) {
-      // cast the abstract state type to the type we expect
-      const ompl::base::DubinsStateSpace::StateType * interpolated_state =
-        path.getState(path_idx)->as<ompl::base::DubinsStateSpace::StateType>();
-
-      geometry_msgs::msg::PoseStamped pose;
-      pose.header.frame_id = polytunnel_cloud_.header.frame_id;
-      pose.header.stamp = rclcpp::Clock().now();
-      pose.pose.position.x = interpolated_state->getX();
-      pose.pose.position.y = interpolated_state->getY();
-      pose.pose.position.z = average_point.z;
-      if (!straight_flag) {
-        pose.pose.orientation = vox_nav_utilities::getMsgQuaternionfromRPY(
-          0, 0, interpolated_state->getYaw());
-      }
-
-      plan_poses.push_back(pose);
-
-    }
-
-    std_msgs::msg::Header header = polytunnel_cloud_.header;
-    vox_nav_utilities::publishClustersCloud(polytunnel_cloud_pub_, header, clusters);
+    vox_nav_utilities::publishClustersCloud(
+      polytunnel_cloud_pub_, polytunnel_cloud_.header,
+      clusters);
 
     RCLCPP_INFO(logger_, "Created a plan with %d pose", plan_poses.size());
 
