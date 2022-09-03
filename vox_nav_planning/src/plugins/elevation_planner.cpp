1c6ab793c0ef1b639ae6b1561aa7152f5fb032c7
jediofgever <fetulahatas1@gmail.com>
2022-09-03 13:36:06 +0200

    sync from sim side

--- vox_nav_planning/src/plugins/elevation_planner.cpp
+++ vox_nav_planning/src/plugins/elevation_planner.cpp
@@ -190,9 +190,10 @@ namespace vox_nav_planning
       planner, planner_name_, si,
       logger_);
 
-    // si->setValidStateSamplerAllocator(std::bind(
-    //     &ElevationPlanner::allocValidStateSampler, this,
-    //     std::placeholders::_1));
+    si->setValidStateSamplerAllocator(
+      std::bind(
+        &ElevationPlanner::allocValidStateSampler, this,
+        std::placeholders::_1));
 
     simple_setup_->setPlanner(planner);
     simple_setup_->setup();
@@ -237,7 +238,10 @@ namespace vox_nav_planning
       }
 
       RCLCPP_INFO(logger_, "Found A plan with %i poses", plan_poses.size());
-      RCLCPP_INFO(logger_, "Path Length, %s, ", planner_name_.c_str(), solution_path.length());
+      RCLCPP_INFO(logger_, "Path Length, %s, %.2f", planner_name_.c_str(), solution_path.length());
+      RCLCPP_INFO(
+        logger_, "Path Smoothness, %s, %.2f",
+        planner_name_.c_str(), solution_path.smoothness());
 
     } else {
       RCLCPP_WARN(logger_, "No solution for requested path planning !");
